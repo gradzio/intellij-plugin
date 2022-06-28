29c6db2704107c977ea6bb0f8e1512940fa41212
GitHub Action <action@github.com>
2022-06-28 06:45:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/gradzio/intellijplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/gradzio/intellijplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.gradzio.intellijplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gradzio.intellijplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/gradzio/intellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/gradzio/intellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.gradzio.intellijplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gradzio.intellijplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
